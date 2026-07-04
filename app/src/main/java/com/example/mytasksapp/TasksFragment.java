fcc77f94a7046a3f3c29a357b6117a2d47435f9a
Matan Amram <matan15455@gmail.com>
2026-07-04 15:46:40 +0300

    Create TasksFragment with ViewBinding

---
+++ app/src/main/java/com/example/mytasksapp/TasksFragment.java
@@ -0,0 +1,33 @@
+package com.example.mytasksapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.mytasksapp.databinding.FragmentTasksBinding;
+
+public class TasksFragment extends Fragment {
+
+    private FragmentTasksBinding binding;
+
+    public TasksFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentTasksBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
