ae71cc9b53aa47be5a83b07b6893860e7ec04c7a
Matan Amram <matan15455@gmail.com>
2026-07-04 16:30:30 +0300

    Create SQLite DatabaseHelper

---
+++ app/src/main/java/com/example/mytasksapp/database/DatabaseHelper.java
@@ -0,0 +1,117 @@
+package com.example.mytasksapp.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "tasks.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_TASKS = "tasks";
+
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_TITLE = "title";
+    private static final String COLUMN_DESCRIPTION = "description";
+    private static final String COLUMN_COMPLETED = "completed";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String createTable = "CREATE TABLE " + TABLE_TASKS + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_TITLE + " TEXT, " +
+                COLUMN_DESCRIPTION + " TEXT, " +
+                COLUMN_COMPLETED + " INTEGER)";
+
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASKS);
+        onCreate(db);
+    }
+
+    public void addTask(Task task) {
+
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_TITLE, task.getTitle());
+        values.put(COLUMN_DESCRIPTION, task.getDescription());
+        values.put(COLUMN_COMPLETED, task.isCompleted() ? 1 : 0);
+
+        db.insert(TABLE_TASKS, null, values);
+        db.close();
+    }
+
+    public ArrayList<Task> getAllTasks() {
+
+        ArrayList<Task> taskList = new ArrayList<>();
+
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_TASKS, null);
+
+        if (cursor.moveToFirst()) {
+
+            do {
+
+                Task task = new Task(
+                        cursor.getInt(0),
+                        cursor.getString(1),
+                        cursor.getString(2),
+                        cursor.getInt(3) == 1
+                );
+
+                taskList.add(task);
+
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+
+        return taskList;
+    }
+
+    public void deleteTask(int id) {
+
+        SQLiteDatabase db = getWritableDatabase();
+
+        db.delete(TABLE_TASKS, COLUMN_ID + "=?",
+                new String[]{String.valueOf(id)});
+
+        db.close();
+    }
+
+    public void updateTask(Task task) {
+
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_TITLE, task.getTitle());
+        values.put(COLUMN_DESCRIPTION, task.getDescription());
+        values.put(COLUMN_COMPLETED, task.isCompleted() ? 1 : 0);
+
+        db.update(TABLE_TASKS,
+                values,
+                COLUMN_ID + "=?",
+                new String[]{String.valueOf(task.getId())});
+
+        db.close();
+    }
+}
