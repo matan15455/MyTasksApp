a34ba4d9eb97e59a0b127cd467c3cb8867c1b615
Matan Amram <matan15455@gmail.com>
2026-07-04 16:25:59 +0300

    Display tasks in RecyclerView

--- app/src/main/java/com/example/mytasksapp/TasksFragment.java
+++ app/src/main/java/com/example/mytasksapp/TasksFragment.java
@@ -6,15 +6,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.mytasksapp.adapter.TaskAdapter;
+import com.example.mytasksapp.database.Task;
 import com.example.mytasksapp.databinding.FragmentTasksBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TasksFragment extends Fragment {
 
     private FragmentTasksBinding binding;
+    private List<Task> taskList;
+    private TaskAdapter adapter;
 
     public TasksFragment() {
-
     }
 
     @Override
@@ -22,6 +29,18 @@ public class TasksFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         binding = FragmentTasksBinding.inflate(inflater, container, false);
+
+        taskList = new ArrayList<>();
+
+        taskList.add(new Task("Buy milk", "Go to the supermarket", false));
+        taskList.add(new Task("Finish homework", "Android project", true));
+        taskList.add(new Task("Wash the car", "Weekend", false));
+
+        adapter = new TaskAdapter(taskList);
+
+        binding.rvTasks.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.rvTasks.setAdapter(adapter);
+
         return binding.getRoot();
     }
 
